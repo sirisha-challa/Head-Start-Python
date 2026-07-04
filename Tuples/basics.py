705d5a0ad01ead6cd70368597aa7eb4e434b9596
Sirisha Challa <iamnoty36@gmail.com>
2026-07-04 13:13:02 +0530

    Updated Local Files and Folders to Repo

---
+++ Tuples/basics.py
@@ -0,0 +1,49 @@
+ages = ("13", "17", "18"
+)
+# print(ages)
+# print(type(ages))
+# ages[1] = 14
+# print(ages)
+
+
+# fruits = ("apple", "banana", "cherry", "apple", "cherry")
+# print(fruits)
+# print(len(fruits))
+
+# fruits = (12,)
+# print(type(fruits))
+
+# Syntax for accessing elements in Sequence data Types - List, Set, Tuple, Dictionary
+# your college => Sequqnce Data Type(List, set, tuple, Dict)
+# Your Pin => Index
+# college = []
+# i = 0
+# while i < 20:
+#     str = f"Std{i}"
+#     college.append(str)
+#     i+=1
+# college = tuple(college)
+# print(college)
+
+# # Syntax for accessing elements in Sequence data Types - List, Set, Tuple, Dictionary
+# # variable_name(college)[index]
+# print(college[3])
+
+# fruits = ("apple", "banana", "cherry")
+# if "guava" in fruits: # Boolean - True / False
+#   print("Yes, 'apple' is in the fruits tuple")
+# else:
+#   print("tHE FRUITS is not in the BAsket")
+
+# i = 0
+# binstr = []
+# while i < 50:
+#     if i % 2 == 0:
+#         binstr.append("1")
+#     else:
+#         binstr.append("0")
+#     i+=1
+# binstr = tuple(binstr)
+# print(binstr)
+# print(binstr.count("1"))
+# print(binstr.count("0"))
